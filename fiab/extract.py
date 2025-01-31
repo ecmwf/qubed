9ff23768ca480dbdd5d051c9913aa87226c6a291
Harrison Cook <harrison.cook@ecmwf.int>
2025-01-31 13:09:28 +0000

    Add frequency

---
+++ fiab/extract.py
@@ -0,0 +1,24 @@
+
+import json
+
+metadata = json.load(open("raw_anemoi_metadata.json"))
+
+predicted_indices = [*metadata['data_indices']['data']['output']['prognostic'], *metadata['data_indices']['data']['output']['diagnostic']]
+variables = metadata['dataset']["variables"]
+variables = [variables[i] for i in predicted_indices]
+
+print('Variables:', variables)
+
+surface_variables = [v for v in variables if '_' not in v]
+pressure_level_variables = [v for v in variables if '_' in v]
+
+pressure_levels = sorted(set([v.split('_')[-1] for v in pressure_level_variables]))
+pressure_level_variables = sorted(set([v.split('_')[0] for v in pressure_level_variables]))
+
+print('Pressure level variables:', pressure_level_variables)
+print('Pressure levels:', pressure_levels)
+
+print('Surface variables:', surface_variables)
+
+frequency = metadata['config']['data']['frequency']
+print("Frequency:", frequency)
