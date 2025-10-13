31986b437bea593ddd650a934503b7a98e4c4017
Mathilde Leuridan <90444327+mathleur@users.noreply.github.com>
2025-10-13 14:57:28 +0200

    fdb scanner script for generating operational qubes w metadata (#35)
    * fdb scanner script for generating operational qubes w metadata
    
    * remove broken scripts

--- fdb_scanner/fdb_dumper_operational.py
+++ fdb_scanner/fdb_dumper_operational.py
@@ -2,43 +2,49 @@
 Blast fdb data with metadata to disk as fast as possible
 """
 
+import time
+import pyfdb
+from pathlib import Path
+import subprocess
 import os
 
 os.environ["ECCODES_PYTHON_USE_FINDLIBS"] = "1"
-os.environ["FDB5_HOME"] = "/home/eouser/fdb_bundle/build"
 
-import subprocess
-from pathlib import Path
-from time import time
 
-import pyfdb
-import yaml
+fdb = pyfdb.FDB()
 
-CONFIG = "config/fdb_config.yaml"
-with open(CONFIG) as f:
-    config = yaml.safe_load(f)
-
-fdb = pyfdb.FDB(config=config)
-
-for year in range(1990, 2050):
-    for month in range(1, 13):
+time0 = time.time()
+for year in [1]:
+    for month in [1]:
+        # SELECTOR = {
+        #     "class" : "od",
+        #     "stream": "oper",
+        #     "expver" : "0001",
+        #     "type": "fc",
+        #     "date": "20251013",
+        #     "time": "0000",
+        #     "levtype": "sfc",
+        # }
         SELECTOR = {
-            "class": "d1",
-            "dataset": "climate-dt",
-            "year": str(year),
-            "month": str(month),
+            "class": "od",
+            "stream": "enfo",
+            "expver": "0001",
+            "type": "pf",
+            "date": "20251013",
+            "time": "0000",
         }
 
-        cf = Path(f"test_scripts/data/climate-dt-flat-{year}-{month}.list.zst")
+        cf = Path(f"oper_fc_enfo-flat-{year}-{month}.list.zst")
         if cf.exists():
             continue
 
-        t0 = time()
-        with open(f"test_scripts/data/climate-dt-flat-{year}-{month}.list", "w") as f:
+        t0 = time.time()
+        with open(f"oper_fc_enfo-flat-{year}-{month}.list", "w") as f:
             # Keep track of the level one and level two keys
             current_level_zero_key = None
             current_level_one_key = None
-            for i, metadata in enumerate(fdb.list(SELECTOR, keys=True, levels=True)):
+            fdb_list = fdb.list(SELECTOR, keys=True, levels=True)
+            for i, metadata in enumerate(fdb_list):
                 level_zero_key = ",".join(
                     f"{k}={v}" for k, v in metadata["keys"][0].items()
                 )
@@ -63,12 +69,16 @@ for year in range(1990, 2050):
                 m = ",".join(f"{k}={metadata[k]}" for k in ["offset", "length"])
                 f.write(f"2 {level_two_key} {m}\n")
                 if i % 2e5 == 0:
-                    print(i, (i + 1) / (time() - t0))
+                    print(i, (i + 1) / (time.time() - t0))
         p = subprocess.run(
-            f"zstd --rm /home/eouser/qubed/test_scripts/data/climate-dt-flat-{year}-{month}.list",
+            f"zstd --rm //home/male/git/qubed/fdb_scanner/oper_fc_enfo-flat-{year}-{month}.list",
             text=True,
             shell=True,
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE,
         )
         print(p)
+time1 = time.time()
+
+print("TIME TAKEN")
+print(time1 - time0)
