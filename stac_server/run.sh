4a39b6e886b1771b4c8cb0638168d11386be8a49
Peter Tsrunchev <peter.tsrunchev@gmail.com>
2025-10-14 00:30:31 +0000

    fix[chart]: deployment

--- stac_server/run.sh
+++ stac_server/run.sh
@@ -1,3 +1,3 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-LOCAL_CACHE=True fastapi dev ./main.py --port 8124 --reload
+API_KEY=asdf LOCAL_CACHE=True uv run fastapi dev ./main.py --port 8124 --reload
