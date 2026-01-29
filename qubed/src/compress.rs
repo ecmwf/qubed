25365eab101d287bf93c4e11748fdf6ab953eb13
mathleur <mathilde.leuridan@ecmwf.int>
2026-01-29 12:01:13 +0100

    add compression test

--- qubed/src/compress.rs
+++ qubed/src/compress.rs
@@ -19,7 +19,7 @@ impl Qube {
     }
 
 
-    fn compress(mut self) {
+    pub fn compress(&mut self) {
         let root = self.root();
         self.compress_recursively(root);
     }
@@ -27,7 +27,7 @@ impl Qube {
     fn compress_recursively(&mut self, node_id: NodeIdx) {
         // First, reccurse into children to get to the leaves
         let children: Vec<NodeIdx> = {
-            let node = self.node_ref(node_id).unwrap();
+            let node = self.node_ref(node_id).expect("Valid nodeIdx in tree");
             node.children()
                 .values()
                 .flat_map(|v| v.iter().copied())
@@ -41,35 +41,53 @@ impl Qube {
         // children are fully compressed so we can hash & merge them
         let children_map = {
             let children = {
-                let node = self.node_ref(node_id).unwrap();
+                let node = self.node_ref(node_id).expect("Valid nodeIdx in tree");
                 node.children().clone()
             };
             self.children_hash_map(&children)
         };
 
+        // for group in children_map.values() {
+        //     let group_dim = self.node_ref(group[0]).unwrap().dim();
+        //     if group.len() > 0 {
+        //         self.merge_coords(*group_dim, group.clone(), node_id);
+        //     }
+        // }
+
         for group in children_map.values() {
-            let group_dim = self.node_ref(group[0]).unwrap().dim();
-            if group.len() > 0 {
-                self.merge_coords(*group_dim, group.clone(), node_id);
+            if group.len() <= 1 {
+                continue; // NOTHING to merge
             }
+
+            let group_dim = self.node_ref(group[0]).expect("Valid nodeIdx in tree").dim();
+            self.merge_coords(*group_dim, group.clone(), node_id);
         }
     }
 
 
     fn merge_coords(&mut self, dim: Dimension, group: Vec<NodeIdx>, node_id: NodeIdx) {
         // Need to get the key dimension
-        let dim_str = self.dimension_str(&dim).unwrap().to_owned();
-        let mut all_coords: Coordinates = self.node_ref(group[0]).unwrap().coords().clone();
+        let dim_str = self.dimension_str(&dim).expect("Valid corresponding dimension string").to_owned();
+        let mut all_coords: Coordinates = self.node_ref(group[0]).expect("Valid child node here that we collected").coords().clone();
         for &node_item in group.iter().skip(1) {
-            all_coords.extend(self.node_ref(node_item).unwrap().coords());
+            all_coords.extend(self.node_ref(node_item).expect("Should have at least 2 nodes in the group").coords());
         }
         // Create new node, which is a child of node_id, which has coords=union all coords in group nodes
         let new_node = self.create_child(&dim_str, node_id, Some(all_coords));
         // Append the children of first node in group to this new node (is fine we choose first node, since all of the nodes should have the same children here anyways)
-        self.add_same_children(new_node.unwrap(), group[1]);
+        self.add_same_children(new_node.expect("just created this node"), group[0]);
         // Remove all of the nodes in group from the tree since they are no longer relevant
+        // self.detach_children(node_id, &group)
         for id in group {
             self.remove_node(id);
+        }
+    }
+
+    fn detach_children(&mut self, parent: NodeIdx, group: &[NodeIdx]) {
+        let parent_node = self.node_mut(parent).unwrap();
+
+        for kids in parent_node.children_mut().values_mut() {
+            kids.retain(|id| !group.contains(id));
         }
     }
 }
